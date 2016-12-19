2bb4e339c7103a3106dfda379a73671984d9c32d
GUYU <1483799299@qq.com>
2016-12-20 07:34:15 +0800

    拦截器初试

---
+++ src/main/java/com/guyu/ssm1/interceptor/StaticInterceptor.java
@@ -0,0 +1,86 @@
+package com.guyu.ssm1.interceptor;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by GUYU on 2016/12/19.
+ */
+public class StaticInterceptor extends HandlerInterceptorAdapter {
+    private final Logger logger = Logger.getLogger(this.getClass());
+    public static final String[] Filters = new String[]{".jsp", ".html"};
+
+
+    /**
+     * 在业务处理器处理请求之前被调用
+     * 如果返回false
+     * 从当前的拦截器往回执行所有拦截器的afterCompletion(),再退出拦截器链
+     * 如果返回true
+     * 执行下一个拦截器,直到所有的拦截器都执行完毕
+     * 再执行被拦截的Controller
+     * 然后进入拦截器链,
+     * 从最后一个拦截器往回执行所有的postHandle()
+     * 接着再从最后一个拦截器往回执行所有的afterCompletion()
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception {
+//        if ("GET".equalsIgnoreCase(request.getMethod())) {
+//            RequestUtil.saveRequest();
+//        }
+        logger.info("==============执行顺序: 1、preHandle================");
+        String requestUri = request.getRequestURI();
+        String contextPath = request.getContextPath();
+        String url = requestUri.substring(contextPath.length());
+
+        logger.info("requestUri:" + requestUri);
+        logger.info("contextPath:" + contextPath);
+        logger.info("url:" + url);
+
+        Boolean beFilter = false;
+        for (String s : Filters) {
+            if (requestUri.lastIndexOf(s) != -1) {
+                beFilter = true;
+                break;
+            }
+        }
+
+        if ( beFilter) {
+            logger.info("Interceptor：跳转到login页面！");
+            request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
+            return false;
+        } else
+            return true;
+    }
+
+    /**
+     * 在业务处理器处理请求执行完成后,生成视图之前执行的动作
+     * 可在modelAndView中加入数据，比如当前时间
+     */
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        logger.info("==============执行顺序: 2、postHandle================");
+        if (modelAndView != null) {  //加入当前时间
+            modelAndView.addObject("var", "测试postHandle");
+        }
+    }
+
+    /**
+     * 在DispatcherServlet完全处理完请求后被调用,可用于清理资源等
+     * <p>
+     * 当有拦截器抛出异常时,会从当前拦截器往回执行所有的拦截器的afterCompletion()
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        logger.info("==============执行顺序: 3、afterCompletion================");
+    }
+
+}
